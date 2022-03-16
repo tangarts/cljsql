3bd5a417fc945f8438f7b9be8224839fcc7248a8
Nehemiah Campbell <nehemiahcampbell@hotmail.co.uk>
2022-03-16 19:01:51 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Let's Build a Simple Database (in Clojure)
+
+
+Following the [cstack](https://cstack.github.io/db_tutorial/) tutorial using lisp with variation
+
+
+
---
+++ repl.clj
@@ -0,0 +1,25 @@
+(ns cstack)
+
+(:exit meta-command)
+
+(defn print-prompt []
+  (prn "db> "))
+
+(defn start-message []
+  (println "SQLite clone v.0.0.1")
+  (println "Enter \".help\" for usage hints")
+  (println "Connected to a transient in-memory database")
+  (println "Use \".open FILENAME\" to re-open a persistent database."))
+
+(defn -main []
+  (start-message)
+  (loop [input (read-line)]
+    (print "sqlite> ")
+    (cond
+      (or (= input ".exit")
+          (= input ".quit")) (print "bye!")
+      :else (do
+              (println input)
+              (recur (read-line))))))
+
+
