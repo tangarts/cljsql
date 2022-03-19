1e6042c290395b487c1ed4c1bfe9ea7cdb8b7df1
Nehemiah Campbell <nehemiahcampbell@hotmail.co.uk>
2022-03-19 08:42:53 +0000

    Add Insert statement using new lexer

--- src/cstack/lexer.clj
+++ src/cstack/lexer.clj
@@ -4,26 +4,15 @@
   {:exit ".exit" :help ".help" :open ".open" :quit ".quit"})
 
 (def keywords
-  {:select  "select"
-   :from    "from"
-   :as      "as"
-   :table   "table"
-   :create  "create"
-   :insert  "insert"
-   :into    "into"
-   :values  "values"
-   :int     "int"
-   :text    "text"})
+  #{"select" "from" "as" "table" "create" "insert" "into" "values" "int" "text"})
 
 (def symbols
-  {:semicolon  ";"
-   :asterisk   "*"
-   :comma      ","
-   :leftparen  "("
-   :rightparen ")"})
+  {:semicolon  ";" :asterisk   "*" :comma      "," :leftparen  "(" :rightparen ")"})
+
+(def token-kind
+  #{:keyword :symbol :identifier :string :number})
 
 (defrecord location [line col])
-(defrecord token [value kind loc])
 (defrecord cursor [pointer loc])
 
 ;;; ==========================================================
@@ -31,7 +20,6 @@
 ;; http://regexadvice.com/forums/thread/29185.aspx
 (def REGEX
   #"(?:(['\"])(?:\\\\|\\\1|(?!\1).|\1\1)*\1|(?:(?<!\d)-)?\d+(?:\.\d+)?(?:[eE]-?\d+)?|\.\.|(?:\w+\.)*\w+|[<>=|]{2}|\S)")
-
 
 (defn digit? [c]
   (re-find #"[0-9]" c))
@@ -48,15 +36,20 @@
 (defn what-type [token]
   (let [c (subs token 0 1)]
     (cond
+      (keywords token) :keyword
       (letter? c)  :word
       (digit? c)   :number
       (asymbol? c) :symbol
       (astring? c) :string
       :else (throw (IllegalArgumentException. (str "Don't know type of token:" token))))))
 
+(keywords "select")
+(what-type "select")
 (defn typer [token]
   {:token token
    :type (what-type token)})
+
+(asymbol? "1- >= 3")
 
 (defn lex [sql-str]
   (map typer (map first (re-seq REGEX sql-str))))
