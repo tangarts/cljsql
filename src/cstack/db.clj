8e181fc8536e87e2696f2d81ed2d8d69637d95d5
Nehemiah Campbell <nehemiahcampbell@hotmail.co.uk>
2022-03-21 10:31:12 +0000

    Draft: db in-memory backend.

--- src/cstack/db.clj
+++ src/cstack/db.clj
@@ -1,53 +1,48 @@
 (ns cstack.db)
 
+; (def db {:customer {:cols [] :types [] :rows [][]}
+;          :tbls2 {:cols [] :types [] :rows [][]}}})
 (defonce db (atom {}))
 
 (defn insert-into
   "adds a new record"
-  [table-name record]
-  (if (@db table-name) (swap! db update-in [table-name] conj record)
+  [statement]
+  (if (@db (statement :table))
+    (swap! db update-in [(->> statement :table) :rows] 
+           conj (zipmap ((get @db (statement :table)) :cols) 
+                        (statement :values)))
       (throw (Exception. "Table does not exist"))))
 
 (defn create-table
-  [table-name]
-  (swap! db assoc (keyword table-name) []))
+  ; TODO add column type validation, for now a table is a vector
+  [statement]
+  (let [dtypes (->> statement :cols (map :datatype) (mapv keyword))
+        cols (->> statement :cols (map :name) (mapv keyword))]
+  (swap! db assoc (statement :name) {:cols cols :types dtypes :rows []})))
+
+(defn select
+  [statement]
+  (->> (get @db (statement :table))
+       :rows
+       (map #(select-keys % (statement :expression)))))
 
 (comment
-  (@db :h)
-
-  (reset! db {})
-  (->> (@db :songs)
-       (map #(select-keys % [:title :artist])))
+  
   @db
-  (create-table "songs")
-
   (do
-    (insert-into :songs {:title "Roses" :artist "Kathy Mattea" :rating 7})
-    (insert-into :songs {:title "Fly" :artist "Dixie Chicks" :rating 8})
-    (insert-into :songs {:title "Home" :artist "Dixie Chicks" :rating 9})
-    (insert-into :songs {:title "Home" :artist "Dixie Chicks" :rating 9})
-    @db
-    (insert-into :hus {:title "Home" :artist "Dixie Chicks" :rating 9})))
-
-(defprotocol Cell
-  (as-text [p])
-  (as-int [p]))
+  (reset! db {})
+  (create-table {:name :customer,
+                 :cols
+                 [{:name "id", :datatype "int"}
+                  {:name "name", :datatype "text"}
+                 {:name "email", :datatype "text"}]})
+  @db
+  (insert-into {:table :customer, :values [1 "'user1'" "'user1@clj.org'"]})  
+  (insert-into {:table :customer, :values [2 "'user2'" "'user2@clj.org'"]})  
+  (insert-into {:table :customer, :values [3 "'user3'" "'user3@clj.org'"]})  
+  @db
+  )
 
 (defn results []
   {:cols {:type nil :name nil}
    :rows nil})
-
-(defprotocol Backend
-  (create-table [statement])
-  (insert [statement])
-  (select [statement]))
-
-
-(defrecord Row [^Integer id ^String username ^String email])
-
-(defn insert-row
-  [expr]
-  (apply ->Row
-         (mapv
-          (fn [v] (convert (v :token) (v :type)))
-          (filter #(not= (-> % :type) :symbol) expr))))
