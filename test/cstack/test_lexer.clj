8ed74b51db01e0ebd33ebb8124d9615ccdaa664a
Nehemiah Campbell <nehemiahcampbell@hotmail.co.uk>
2022-03-23 09:26:58 +0000

    Add lexer and parser golden test

---
+++ test/cstack/test_lexer.clj
@@ -0,0 +1,44 @@
+(ns cstack.test-lexer
+  (:require [clojure.test :refer :all]
+            [cstack.lexer :refer [lex typer]]))
+
+(deftest lex-number-type
+  (are [x] (= ((typer x) :type) :number)
+       ; .3 FAILS
+    "1" "1.03" "0.03" "100" "1e3" "1E4"))
+
+
+(deftest lex-keyword-type
+  (are [x] (= ((typer x) :type) :string)
+    "'user'"
+    "\"GBP\""))
+
+(deftest lex-symbol-type
+  (are [x] (= ((typer x) :type) :symbol)
+    ";" "*" "<>" "<=" "<=" "*" "="))
+
+(deftest lex-identifier-type
+  (is (= ((typer "t") :type) :identifier))
+  (is (= ((typer "steven") :type) :identifier)))
+
+(deftest full-lexer
+  (is (= (lex "INSERT INTO users VALUES ('Stephen', 16);")
+         [{:token "insert", :col 0, :type :identifier}
+          {:token "into", :col 6, :type :identifier}
+          {:token "users", :col 10, :type :identifier}
+          {:token "values", :col 15, :type :identifier}
+          {:token "(", :col 21, :type :symbol}
+          {:token "'stephen'", :col 22, :type :string}
+          {:token ",", :col 31, :type :symbol}
+          {:token "16", :col 32, :type :number}
+          {:token ")", :col 34, :type :symbol}
+          {:token ";", :col 35, :type :symbol}])))
+
+(comment
+  (def query "create table customer (id int, name text);")
+  (def sql-str "select id, username from places where locality=\"los angeles\";")
+
+  (lex "SELECT age + 2, name FROM users WHERE age = 23;")
+  (lex "SELECT name FROM users;"))
+
+(run-tests 'cstack.test-lexer)
---
+++ test/cstack/test_parser.clj
@@ -0,0 +1,45 @@
+(ns cstack.test-parser
+  (:require [clojure.test :refer :all]
+            [cstack.parser :refer :all]))
+
+(deftest test-parse-select
+  (are [x y] (= x y)
+    (parse-select [{:token "select", :type :keyword}
+                   {:token "id", :type :identifier}
+                   {:token "from", :type :keyword}
+                   {:token "t", :type :identifier}])
+    {:table :t, :expression [:id]}
+
+    (parse-select [{:token "select", :type :keyword}
+                   {:token "id", :type :identifier}
+                   {:token ",", :type :symbol}
+                   {:token "username", :type :identifier}
+                   {:token "from", :type :keyword}
+                   {:token "customer", :type :identifier}])
+    {:table :customer, :expression [:id :username]}))
+
+(deftest test-parse-create
+  (are [x y] (= x y)
+    (parse-create
+     [{:token "create", :type :keyword}
+      {:token "table", :type :keyword}
+      {:token "customer", :type :identifier}
+      {:token "(", :type :symbol}
+      {:token "id", :type :identifier}
+      {:token "int", :type :keyword}
+      {:token ")", :type :symbol}
+      {:token ";", :type :symbol}])
+    {:name :customer, :cols [{:name "id", :datatype "int"}]}))
+
+(run-tests 'cstack.test-parser)
+
+(comment
+  (parse-exprs [{:token "id", :type :identifier}
+                {:token "int", :type :keyword}
+                {:token ",", :type :symbol}
+                {:token "name", :type :identifier}
+                {:token "text", :type :keyword}
+                {:token ")", :type :symbol}
+                {:token ";", :type :symbol}] ")")
+  [])
+
