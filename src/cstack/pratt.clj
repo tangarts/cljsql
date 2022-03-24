f647322212e4a6ada5e007658e38d501a0319f02
Nehemiah Campbell <nehemiahcampbell@hotmail.co.uk>
2022-03-24 22:26:31 +0000

    Sample code for pratt parsing. TODO: fix my implementation

--- src/cstack/pratt.clj
+++ src/cstack/pratt.clj
@@ -11,113 +11,54 @@
   [& body]
   `(binding [*verbose* true] ~@body))
 
-;;; function parse(tokens):
-;    firstToken = tokens.consume()
-;
-;    if firstToken is a number
-;        leftNode = NumberNode(firstToken)
-;    otherwise
-;        Error("Expected a number")
-;
-;    nextToken = tokens.consume()
-;
-;    if nextToken is an operator token
-;        rightNode = parse(tokens)
-;
-;        return OperatorNode(
-;            operator: nextToken,
-;            leftNode,
-;            rightNode
-;        )
-;
-;    otherwise
-;        return leftNode
-
-; [1, "/", 2, "+", 3.4].consume() -> 1, ["/", 2, "+", 3.4]
-; [1, "/", 2, "+", 3.4].peek() -> 1, [1, "/", 2, "+", 3.4]
-; 
-; [].consume() -> empty token, []
-; [].peek() -> empty token, []
-
 (def op #{'+ '- '* '/})
-(op '+)
 
-(def precedence '{* 3, / 3 + 1, - 1})
+(defn prec [op]
+  (case op
+    (+ -) [1 2]
+    (* /) [3 4]
+    (println "Not an op")))
 
-(declare parse)
-(defn- -parse [& tokens]
-  (let
-   [[t tn] tokens]
-    (if
-     (nil? tn) (first t)
-     (loop [[t tn & ts] tokens
-            acc t
-            minbp 0]
-       (println "t:" t)
-       (println "tn:" tn)
-       (println "ts:" ts)
-       (println "acc: " acc)
-       (cond
-         (nil? tn) {:kind :binary
-                    :expressions acc}
-         ;  (number? t) (recur ts (update acc :expression 
-         ;                                conj {:op tn
-         ;                                   :left t
-         ;                                   :right (-parse ts)}))
-         (<= (precedence tn) minbp)
-         {:kind :binary :expressions acc}
+(second [1 2])
 
-         (op tn)
-         (let [r (-parse ts)]
-           (println "r:" r)
-           (recur ts (cons tn ; {:op tn :left t :right (-parse ts)})
-                           (list acc r))
-                  (inc (precedence tn)))))))))
+(defn multi [tokens]
+  (let [[t tn & ts] tokens]
+    (list t tn ts)))
 
-(defn multi [& tokens]
-  (let [[t tn] tokens]
-    (list t tn)))
+(multi '(1))
 
-(defn parse [tokens]
-  (let [t (first tokens) 
-        tn (rest tokens)]
-    (if
-     (nil? tn) t
-     (loop [[t tn & ts] tokens
-            acc t
-            minbp 0]
-       (println "t:" t)
-       (println "tn:" tn)
-       (println "ts:" ts)
-       (println "acc: " acc)
-       (cond
-         (nil? tn) {:kind :binary
-                    :expressions acc}
+(defn parse [tokens minbp]
+  ; (let [t (first tokens) 
+  ;       tn (rest tokens)]
+  ;   (if (nil? tn) t)
+  ;   )
+  (loop [[t tn & ts] tokens
+         acc t]
+    (println "t:" t)
+    (println "tn:" tn)
+    (println "ts:" ts)
+    (println "acc: " acc)
+    (cond
+      (nil? tn) {:kind :binary
+                 :expressions acc}
 
-        (<= (precedence tn) minbp)
-         {:kind :binary :expressions acc}
+      (op tn)
+      (let [[lbp rbp] (prec tn)]
+        (if (< lbp minbp) {:kind :binary :expressions acc}
 
-         (op tn)
-         (let [r (:expressions (parse ts))]
-           (println "r:" r)
-           (recur ts (cons tn ; {:op tn :left t :right (-parse ts)})
-                           (list acc r))
-                  (inc (precedence tn)))))))))
+            (let [r (:expressions (parse ts rbp))]
+              (println "r:" r)
+              (println "count ts " (count ts))
+              (recur ts (cons tn ; {:op tn :left t :right (-parse ts)})
+                              (list acc r))))))
+      :else
+      acc)))
 
-(parse '(1))
-(parse '(1 * 2))
-(parse '(3 * 2 + 1))
-(parse [1 '+ 2 '* 3])
-(parse '(a + b * c * d + e))
-(parse '(2 * 3))
-
-(def maps [{:op '*, :left 4, :right 1}
-           {:op '+, :left 1, :right 2}
-           {:op '*, :left 2, :right 3}])
-
-(-> (first maps)
-    (assoc-in (into [] (repeat 1 :right)) (get maps 1))
-    (assoc-in (into [] (repeat 2 :right)) (get maps 2)))
+(parse '(1) 0)
+(parse '(1 * 2) 0)
+(parse '(3 * 2 + 1) 0) ; {:kind :binary, :expressions (+ (* 3 2) 1)}
+(parse '(1 + 2 * 3) 0)
+(parse '(a + b * c * d + e) 0)
 
 (defn infix [& exprs]
   (loop [[x & xs] exprs
@@ -128,3 +69,43 @@
       (number? x) (recur xs (oper acc x) nil)
       :else (recur xs acc x))))
 
+(defn reorder-equation [arg]
+  (if (seq? arg)
+    (let [[f s & r] arg
+          f (reorder-equation f)]
+      (cond
+        (#{"*" "/"} (str s))
+        (let [[t ft & r2] r
+              t (reorder-equation t)]
+          (if ft
+            (list ft (list s f t) (reorder-equation r2))
+            (list s f t)))
+        (nil? s) f
+        :else (list s f (reorder-equation r))))
+    arg))
+
+(reorder-equation '(a + b * c * d + e))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; https://github.com/fogus/unfix/blob/master/src/joy/unfix/infix.clj
+
+(def && #(and   %1 %2))
+(def || #(or    %1 %2))
+(def != #(not=  %1 %2))
+
+(def ^:dynamic *ops* '[- + * / < > && || = !=])
+(def rank (zipmap *ops* (iterate inc 1)))
+; (def ^:dynamic _ clojure.core/resolve)
+
+(defn- infix* 
+  [[a b & [c d e & more] :as _]]
+  (cond
+   (vector? a) (recur (list* (infix* a) b c d e more))
+   (vector? c) (recur (list* a b (infix* c) d e more))
+   (rank b)    (if (and d (< (rank b 0) (rank d 0)))
+                 (recur (list a b (infix* (list* c d e more))))
+                 (recur (list* (list b a c) d e more)))
+   :else a))
+
+(infix* '[a + b * c * d + e])
