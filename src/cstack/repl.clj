7847e3ae6f029a818d8841c200d48ab9d0cb09da
Nehemiah Campbell <nehemiahcampbell@hotmail.co.uk>
2022-03-27 10:46:38 +0100

    Add support for .help meta command and future meta commands.

--- src/cstack/repl.clj
+++ src/cstack/repl.clj
@@ -1,6 +1,6 @@
 (ns cstack.repl
   (:require [cstack.lexer :refer [lex]]
-            [cstack.db :refer [execute]]
+            [cstack.db :refer [execute db]]
             [cstack.parser :refer [parse-statement]]))
 
 (defn start-message []
@@ -9,17 +9,31 @@
   (println "Connected to a transient in-memory database")
   (println "Use \".open FILENAME\" to re-open a persistent database."))
 
+(defn print-help []
+  (println ".exit\t\t Exit this program")
+ ; (println ".open ?FILE?\t\t Close existing database and reopen FILE")
+  (println ".quit\t\t Exit this program")
+ ; (println ".read FILE\t\t Read input from FILE")
+ ; (println ".save FILE\t\t Write in-memory database into FILE")
+  (println ".tables\t\t List available tables"))
+
+(defn execute-meta [token]
+  (case token
+    "tables" (apply println (map name (keys @db)))
+    "help" (print-help)
+    "save" (println "To implement: .save")
+    (println "Unrecognized command" token)))
+
 (defn -main []
   (start-message)
   (loop []
-    (print "sqlite> ")
+    (print "cljsql> ")
     (flush)
     (let [tokens (lex (read-line))]
       (if (= (-> tokens first :token) ".")
-        (if (or (= "exit" (-> tokens second :token))
-                (= "quit" (-> tokens second :token))) (println "bye!")
+        (if (#{"exit" "quit"} (-> tokens second :token)) (print "bye!")
             (do
-              (println "Unrecognized command" (-> tokens second :token))
+              (execute-meta (-> tokens second :token))
               (recur)))
         (do
           (execute (parse-statement tokens))
