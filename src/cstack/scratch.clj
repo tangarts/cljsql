81cbaac463234f4d0bb2c0a1e9fa12cf774336ad
Nehemiah Campbell <nehemiahcampbell@hotmail.co.uk>
2022-03-28 18:33:06 +0100

    Clean up scatch file.

--- src/cstack/scratch.clj
+++ src/cstack/scratch.clj
@@ -1,7 +1,7 @@
-(ns cljsexp-simple.core
+(ns cstack.scratch
   (:require [clojure.pprint :as p]
-            [clojure.walk :refer [postwalk]]))
-
+            [clojure.walk :refer [postwalk]]
+            [clojure.test :refer :all]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -19,10 +19,6 @@
           {:token "2", :type :number}])
 
 (def precedence '{* 0, / 0 + 1, - 1})
-
-(defn node [x op y]
-  {:kind :binary
-   :expr {:a x :op op :b y}})
 
 (defn order-ops
   "((A x B) y C) or (A x (B y C)) depending on precedence of x and y"
@@ -53,3 +49,72 @@
  (-> '(3 * 2 + 1) add-parens))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def op '#{and or = <> < > <= >= || +})
+
+(defn binding-power [op]
+  (case op
+    (and or) 1
+    (= <>) 2
+    (< >) 3
+    (<= >=) 4
+    (|| +) 5
+    0))
+
+(defn node [op x y]
+  {:kind :binary
+   :expr {:a x :op op :b y}})
+
+(defn parse-binary
+  "Parse Binary expression"
+  [tokens]
+  (if (seq? tokens)
+    (loop [[f s & r] tokens]
+      (if (nil? s) f
+          (let [[t ft & _] r]
+            (cond
+
+              (op s)
+              (if (and ft (< (binding-power s) (binding-power ft)))
+                (node s f (parse-binary r))
+                (recur (list* (node s f t) (rest r))))
+
+              :else
+              ; (node s f (calc r))
+              (println "Binary expression expected")))))
+    tokens))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(parse-binary '(1))
+(parse-binary '(1 + 2))
+(p/pprint (parse-binary '(1 + 2 and 3 > 4)))
+(p/pprint
+ (parse-binary '(select * from t where id = 1 and c > d + e)))
+
+(p/pprint
+ (parse-binary '(a + b and c + d <> e)))
+
+(deftest pratt-parse
+  (are [x y] (= (parse-binary x) y)
+    '(1) 1
+    '(1 + 2) '(+ 1 2)
+    '(1 + 2 * 3) '(+ 1 (* 2 3))))
+
+(run-tests 'cstack.pratt)
+
+(comment
+; https://github.com/fogus/unfix/blob/master/src/joy/unfix/infix.clj
+
+  (def ^:dynamic *ops* '[- + * / < > and or = <>])
+  (def rank (zipmap *ops* (iterate inc 1)))
+
+  (defn- infix*
+    [[a b & [c d e & more] :as _]]
+    (cond
+      (vector? a) (recur (list* (infix* a) b c d e more))
+      (vector? c) (recur (list* a b (infix* c) d e more))
+      (rank b)    (if (and d (< (rank b 0) (rank d 0)))
+                    (recur (list a b (infix* (list* c d e more))))
+                    (recur (list* (list b a c) d e more)))
+      :else a)))
